d22a715e40176dac3fee8d2319968c0f75043109
Chris Janzen <chradajan@gmail.com>
2015-08-15 13:40:05 -0700

    Took out servo object

--- Combined Sketches/Baro_Honeywell_Combo_Test.ino
+++ Combined Sketches/Baro_Honeywell_Combo_Test.ino
@@ -1,7 +1,17 @@
 #include <Wire.h>
 #include <SSC.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+
+int Addr = 105;                 // I2C address of gyro
+int x, y, z;
 
 SSC ssc(0x28, 8);
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 const byte CoeffNum = 6;
 unsigned long coefficients_[6];    // use long rather than int for calculations
@@ -30,17 +40,19 @@ void setup()
   ssc.setMaxRaw(16383);
   ssc.setMinPressure(0.0);
   ssc.setMaxPressure(1.6);
+  accel.begin();
+  accel.setRange(ADXL345_RANGE_16_G);
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  writeI2C(CTRL_REG1, 0x1F);    // Turn on all axes, disable power down
+  writeI2C(CTRL_REG3, 0x08);    // Enable control ready signal
+  writeI2C(CTRL_REG4, 0x80);    // Set scale (500 deg/sec)
+  delay(100);                   // Wait to synchronize 
+  
 
   //  start the sensor
   Serial.print("start()\t\t");
   Serial.println(ssc.start());
-  
-  
-  myservo.attach(7);  // attaches the servo on pin 9 to the servo object 
-  
-  Serial.begin(115200);
- // Serial.println(Version);
-  Serial.println();
   
   // Reset MS5607 Sensor
   Wire.begin();
@@ -110,9 +122,54 @@ void DoInDelay() {
   }
 }
 
+void getGyroValues () {
+  byte MSB, LSB;
+
+  MSB = readI2C(0x29);
+  LSB = readI2C(0x28);
+  x = ((MSB << 8) | LSB);
+
+  MSB = readI2C(0x2B);
+  LSB = readI2C(0x2A);
+  y = ((MSB << 8) | LSB);
+
+  MSB = readI2C(0x2D);
+  LSB = readI2C(0x2C);
+  z = ((MSB << 8) | LSB);
+}
+
+int readI2C (byte regAddr) {
+    Wire.beginTransmission(Addr);
+    Wire.write(regAddr);                // Register address to read
+    Wire.endTransmission();             // Terminate request
+    Wire.requestFrom(Addr, 1);          // Read a byte
+    while(!Wire.available()) { };       // Wait for receipt
+    return(Wire.read());                // Get result
+}
+
+void writeI2C (byte regAddr, byte val) {
+    Wire.beginTransmission(Addr);
+    Wire.write(regAddr);
+    Wire.write(val);
+    Wire.endTransmission();
+}
+
 void loop() 
 {
+    sensors_event_t event; 
+    accel.getEvent(&event);
+    Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("   ");
+    Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("   ");
+    Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("   ");
+    Serial.println();
+    
+    getGyroValues();
+    Serial.print("Raw X:");  Serial.print(x/114);  
+    Serial.print(" Raw Y:"); Serial.print(y/114);
+    Serial.print(" Raw Z:"); Serial.println(z/114);
+    
     ssc.update(); 
     Serial.println(ReadADC(0x11));   // typical value 8077636 
     Serial.println(ReadADC(0x01));  // typical value 646544
+    Serial.println();
 }
